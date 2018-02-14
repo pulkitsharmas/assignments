8e4088846fa2c3f91d887c6be65b871486b31196
pulkitsharmas <pku996@gmail.com>
2018-02-14 17:21:49 +0530

    routing set-up; created default to route login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './login/login.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-users.ts
@@ -0,0 +1,6 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+  {email: 'support@mystore.com', password: 'support', name: 'Support User'},
+  {email: 'admin@mystore.com', password: 'admin', name: 'Administrator'}
+];
