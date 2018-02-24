d017d6e2537162718f36dc2676fa52b1aa5e9c20
Pulkit Sharma <pku996@gmail.com>
2018-02-24 18:48:54 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { ITEMS } from '../mock-inventory';
 import {Item} from '../item';
 import {MatTableDataSource} from '@angular/material';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
-import {ItemDetailViewComponent} from '../item-detail-view/item-detail-view.component';
+
 
 
 @Component({
@@ -20,19 +20,8 @@ export class HomeComponent implements OnInit {
     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
     this.dataSource.filter = filterValue;
   }
-  openDialog(): void {
-    let dialogRef = this.dialog.open(ItemDetailViewComponent, {
-      width: '1250px',
-      data: {}
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.animal = result;
-    });
-  }
-
-  constructor(public dialog: MatDialog) { }
+  
+  constructor() { }
 
   ngOnInit() {
   }
